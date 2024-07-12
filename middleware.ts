d9a20d9a234a4bc6a9f6ca10b96fe474c7667c8e
thekaushalmore <55kaushalmore@gmail.com>
2024-07-12 17:21:37 +0530

    first commit

---
+++ middleware.ts
@@ -0,0 +1,5 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+export default clerkMiddleware((auth, req) => { if (isProtectedRoute(req)) auth().protect(); }); 
+
+const isProtectedRoute = createRouteMatcher([ '/' ]) 
+export const config = { matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],};
